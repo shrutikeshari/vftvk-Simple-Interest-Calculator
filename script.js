6ba2f3616a7d4b9e781f4c7ade729b9aaee460b6
shrutikeshari <79959913+shrutikeshari@users.noreply.github.com>
2021-03-16 17:53:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function compute()
+{ var p = parseInt(document.getElementById("principal").value);
+if(p==0)
 {
+alert("please enter positive number");
+}
+else {
 document.getElementById('result').style.display="block";
-   var p = parseInt(document.getElementById("principal").value);
+   
 var r= parseFloat(document.getElementById("rate").value);
 var y=parseInt(document.getElementById("year").value);
 var total=(p*r*y)/100;
@@ -11,6 +16,7 @@ document.getElementById("span1").innerHTML=p+".";
 document.getElementById("span2").innerHTML=r+"%.";
 document.getElementById("span3").innerHTML=total+".";
 document.getElementById("span4").innerHTML=yr+".";
+}
 }
 function changeRate(val) {
 document.getElementById("range1").innerHTML=val+"%";
