cb1b22daed7a07fb64298b25d35c8b4eb9924771
shrutikeshari <79959913+shrutikeshari@users.noreply.github.com>
2021-03-16 17:43:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+document.getElementById('result').style.display="block";
+   var p = parseInt(document.getElementById("principal").value);
+var r= parseFloat(document.getElementById("rate").value);
+var y=parseInt(document.getElementById("year").value);
+var total=(p*r*y)/100;
+var d=new Date();
+var yr=parseInt(d.getFullYear())+y;
+document.getElementById("span1").innerHTML=p+".";
+document.getElementById("span2").innerHTML=r+"%.";
+document.getElementById("span3").innerHTML=total+".";
+document.getElementById("span4").innerHTML=yr+".";
+}
+function changeRate(val) {
+document.getElementById("range1").innerHTML=val+"%";
+}
+function display() {
+document.getElementById("result").style.display="none";
 }
         
